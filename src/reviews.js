b5d0fcdd7992a2057306ac8af5c3bdca6b3a62f8
Liza Nauman <naumanes@mail.ru>
2024-11-08 23:42:35 +0300

    first commit

---
+++ src/reviews.js
@@ -0,0 +1,24 @@
+export const initialReviews = [
+    {
+        name: 'What I talk about when I talk about running',
+        image: "https://images.squarespace-cdn.com/content/v1/5fea3b3d46d31b1ddd850cf5/1617984349371-0MVPHNPR7WU1P9IU90MX/1_cApQ5sl_OaVJMwHFpoOc1Q.jpeg?format=2500w", 
+        author: 'Haruki Murakami',
+        description: "Haruki Murakami's 'What I Talk About When I Talk About Running' explores the connection of the writer’s strategies in writing and running. Through his personal experience, Murakami reveals how the discipline of long-distance running parallels the art of writing. He highlights the importance of routine, perseverance, and the challenges that come with both practices. Murakami's book offers insights on finding inspiration and the significance of commitment to both art and sport. His observations about pain, solitude, and stamina resonate deeply, inspiring readers to embrace their own creative journeys. Overall, the book serves not only as a memoir but also as a guide for aspiring writers seeking motivation and wisdom, in order to expand their writing abilities.",
+        rating: 5
+    },
+    {
+        name: "Perfume: The Story of a Murderer",
+        image: "https://cdn1.ozone.ru/s3/multimedia-1-4/7128746932.jpg",
+        author: "Patrick Süskind",
+        description: '"Perfume: The Story of a Murderer" is a hauntingly unique exploration of obsession and self-identity through the life of Jean-Baptiste Grenouille, a peculiar man born with an extraordinary sense of smell, but with no human scent himself. The narrative immerses readers in the setting of 18th-century France, which Süskind depicts with vivid and very grotesque detail. While the story’s rather graphic gore can be unsettling and nausea-inducing - offering a glimpse into the ‘filthy France’ of that time period, or depicting Jean-Baptiste’s killings - it is precisely this intensity that makes the character’s journey so compelling to read. His search of the perfect scent, leading to ruthless acts of murder, raises questions about humanity and the nature of desire and passion.',
+        rating: 4
+    },
+    {
+        name: "Catcher in the Rye",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/The_Catcher_in_the_Rye_%281951%2C_first_edition_cover%29.jpg/1032px-The_Catcher_in_the_Rye_%281951%2C_first_edition_cover%29.jpg",
+        author: "J.D. Salinger",
+        description: 'Catcher in the Rye was quite a disappointing read. The constant use of profanity by the protagonist felt excessive and unnecessary to the story. Although the inclusion of vulgar language and some mild «adult»  themes do reflect Holden Caulfield’s (main character) angst and introduction to new experiences as a teenage boy, his behaviour and overall personality come across as forced and juvenile, making the novel hard to enjoy.',
+        rating: 2
+    }
+]
+
